5c9b91195491dd5b6c5ce54d883089fea662bbd8
Taufik Pragusga <taufik@pragusga.com>
2024-02-25 17:42:38 +0700

    ..

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { BACKEND_URL } from './credentials';
 
 export const AxiosClient = axios.create({
-  baseURL: 'http://' + BACKEND_URL,
+  baseURL: BACKEND_URL,
   headers: {
     Accept: '*/*',
     'Content-Type': 'application/json',
--- frontend/lib/credentials.ts
+++ frontend/lib/credentials.ts
@@ -1,2 +1,5 @@
-export const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN ?? "pk.eyJ1IjoiemlkYW5raGFyaXNtYSIsImEiOiJjbG8zemN5cWswMnBpMmpudXEyYWZta2M3In0.SnGCAMJEYUBkaGMwZCQMpA";
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL ?? "localhost:8080";
+export const MAPBOX_TOKEN =
+  process.env.NEXT_PUBLIC_MAPBOX_TOKEN ??
+  'pk.eyJ1IjoiemlkYW5raGFyaXNtYSIsImEiOiJjbG8zemN5cWswMnBpMmpudXEyYWZta2M3In0.SnGCAMJEYUBkaGMwZCQMpA';
+export const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:8080';
