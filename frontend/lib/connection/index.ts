39f9f191535d2a133cae7c5531296c39c7dc39e6
Taufik Pragusga <taufik@pragusga.com>
2024-02-26 06:20:26 +0700

    ws

--- frontend/lib/connection/index.ts
+++ frontend/lib/connection/index.ts
@@ -1,10 +1,13 @@
-import { BACKEND_URL } from "../credentials";
+import { WS_URL } from '../credentials';
 
-export const createWSConnection = (str: string, cb: (message: MessageEvent<any>) => void) => {
-  const socket = new WebSocket(`ws://${BACKEND_URL}${str}`);
-  console.log("CREATEWSConnection")
+export const createWSConnection = (
+  str: string,
+  cb: (message: MessageEvent<any>) => void
+) => {
+  const socket = new WebSocket(`${WS_URL}${str}`);
+  console.log('CREATEWSConnection');
   socket.onopen = () => {
-    console.log("Connect");
+    console.log('Connect');
   };
   socket.onmessage = cb;
   socket.onclose = (close) => {
--- frontend/lib/credentials.ts
+++ frontend/lib/credentials.ts
@@ -3,3 +3,4 @@ export const MAPBOX_TOKEN =
   'pk.eyJ1IjoiemlkYW5raGFyaXNtYSIsImEiOiJjbG8zemN5cWswMnBpMmpudXEyYWZta2M3In0.SnGCAMJEYUBkaGMwZCQMpA';
 export const BACKEND_URL =
   process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:8080';
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL ?? 'ws://localhost:8080';
